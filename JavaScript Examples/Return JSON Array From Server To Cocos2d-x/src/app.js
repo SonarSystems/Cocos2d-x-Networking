30f081f3dd6987267e5104467e7238ded215f997
Frahaan Hussain <Sonar Systems>
2015-11-19 11:50:03 +0000

    Return JSON Array Sample Code

---
+++ JavaScript Examples/Return JSON Array From Server To Cocos2d-x/src/app.js
@@ -0,0 +1,47 @@
+// ANDROID requires
+// <uses-permission android:name="android.permission.INTERNET"/>
+
+// DON'T Forget to add "App Transport Security Settings" to Info.plist
+// DON'T Forget to add "Allow Arbitrary Loads" under the previous section added inside of Info.plist and set it to YES
+
+var HelloWorldLayer = cc.Layer.extend({
+    ctor:function () {
+        //////////////////////////////
+        // 1. super init first
+        this._super();
+    
+        var xhr = cc.loader.getXMLHttpRequest();
+        
+        xhr.open( "GET", "http://www.sonarlearning.co.uk/extras/cocostutorial/jsonarray.php" );
+        xhr.setRequestHeader( "Content-Type", "text/plain" );
+        xhr.send( );
+                                      
+        xhr.onreadystatechange = function ()
+        {
+            cc.log( "Networking away" );
+                                      
+            if ( xhr.readyState == 4 && ( xhr.status >= 200 && xhr.status <= 207 ) )
+            {
+                var httpStatus = xhr.statusText;
+                cc.log( httpStatus );
+
+                var jsonData = JSON.parse(xhr.responseText);
+
+                cc.log( "HTTP Response : key a : " + jsonData["a"] );
+                cc.log( "HTTP Response : key b : " + jsonData["b"] );
+                cc.log( "HTTP Response : key c : " + jsonData["c"] );
+            }
+        }
+                                      
+        return true;
+    },
+});
+
+var HelloWorldScene = cc.Scene.extend({
+    onEnter:function () {
+        this._super();
+        var layer = new HelloWorldLayer();
+        this.addChild(layer);
+    }
+});
+
